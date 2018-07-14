5ed04ecbc37258a212ab65ffb5a8b98aba7ede6c
hejiaming <jamisonline.he@gmail.com>
2018-07-14 20:02:39 +0800

    新增车辆表

--- ngls-module-user/src/main/java/com/ngls/user/domain/Car.java
+++ ngls-module-user/src/main/java/com/ngls/user/domain/Car.java
@@ -52,6 +52,111 @@ public class Car implements Serializable {
     @Column(columnDefinition = "varchar(50) comment '被保人'")
     public String insuredName;
 
-    @Column(columnDefinition = "comment '保险公司Id'")
+    @Column(columnDefinition = "int(50) comment '保险公司Id'")
     public Long companyId;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLicenseNo() {
+        return licenseNo;
+    }
+
+    public void setLicenseNo(String licenseNo) {
+        this.licenseNo = licenseNo;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getCarVin() {
+        return carVin;
+    }
+
+    public void setCarVin(String carVin) {
+        this.carVin = carVin;
+    }
+
+    public String getEngineNo() {
+        return engineNo;
+    }
+
+    public void setEngineNo(String engineNo) {
+        this.engineNo = engineNo;
+    }
+
+    public int getRenewalCarType() {
+        return renewalCarType;
+    }
+
+    public void setRenewalCarType(int renewalCarType) {
+        this.renewalCarType = renewalCarType;
+    }
+
+    public String getLicenseOwner() {
+        return licenseOwner;
+    }
+
+    public void setLicenseOwner(String licenseOwner) {
+        this.licenseOwner = licenseOwner;
+    }
+
+    public String getOwnerPhone() {
+        return ownerPhone;
+    }
+
+    public void setOwnerPhone(String ownerPhone) {
+        this.ownerPhone = ownerPhone;
+    }
+
+    public String getSixDigitsAfterIdCard() {
+        return sixDigitsAfterIdCard;
+    }
+
+    public void setSixDigitsAfterIdCard(String sixDigitsAfterIdCard) {
+        this.sixDigitsAfterIdCard = sixDigitsAfterIdCard;
+    }
+
+    public int getCarUsedType() {
+        return carUsedType;
+    }
+
+    public void setCarUsedType(int carUsedType) {
+        this.carUsedType = carUsedType;
+    }
+
+    public String getPostedName() {
+        return postedName;
+    }
+
+    public void setPostedName(String postedName) {
+        this.postedName = postedName;
+    }
+
+    public String getInsuredName() {
+        return insuredName;
+    }
+
+    public void setInsuredName(String insuredName) {
+        this.insuredName = insuredName;
+    }
+
+    public Long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Long companyId) {
+        this.companyId = companyId;
+    }
 }
