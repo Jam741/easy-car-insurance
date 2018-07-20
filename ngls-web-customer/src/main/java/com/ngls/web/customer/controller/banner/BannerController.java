2897e84512ca983a24d11a0106fb781b9bc49af0
Kixs <emp_kixs@163.com>
2018-07-20 22:29:43 +0800

    json

--- ngls-web-customer/src/main/java/com/ngls/web/customer/controller/banner/BannerController.java
+++ ngls-web-customer/src/main/java/com/ngls/web/customer/controller/banner/BannerController.java
@@ -4,7 +4,6 @@ package com.ngls.web.customer.controller.banner;
 import com.ngls.common.util.mvc.support.ApiResult;
 import com.ngls.user.dto.BannerDto;
 import com.ngls.user.service.BannerService;
-import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,24 +17,24 @@ public class BannerController {
     @Autowired
     BannerService bannerService;
 
-    @GetMapping(name = "/list")
+    @GetMapping
     public ApiResult<List<BannerDto>> listBanner() {
         return bannerService.findAll();
     }
 
 
-    @PostMapping(name = "/banner")
+    @PostMapping
     public ApiResult<BannerDto> save(@RequestBody BannerDto bannerDto) {
         return bannerService.save(bannerDto);
     }
 
-    @GetMapping(name = "/banner/{id}")
-    public ApiResult<BannerDto> findOne(@PathVariable Long id) {
+    @GetMapping("{id}")
+    public ApiResult<BannerDto> findOne(@PathVariable(name = "id") Long id) {
         return bannerService.findOne(id);
     }
 
-    @DeleteMapping(name = "/banner")
-    public ApiResult delete(@ApiParam Long id) {
+    @DeleteMapping
+    public ApiResult delete(@RequestParam("id") Long id) {
         return bannerService.delete(id);
     }
 
